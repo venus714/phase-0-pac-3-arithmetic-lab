8430b4cba921b2dc2fb7fc58a911fc5b9a42490d
Student <venus.kabera@student.moringaschool.com>
2022-11-19 14:36:59 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+let x=20
+let y=20
+
+function add(x,y ){
+return x+y
+
+}
+function subtract(x,y){
+    return x-y
+}
+function multiply(x,y){
+    return x*y
+}
+function divide(x,y){
+return x/y
+
+}
+
+function increment (x){
+
+return ++x
+}
+function decrement (y){
+    return --y
+}
+function makeInt(x){
+
+return parseInt (x,10)}
+
+
+function preserveDecimal(y){
+    return parseFloat(y)
+  }
